4fa19234d02add5c9ec2eab98629e68e789fb16a
Tareq <tareqnmd@gmail.com>
2024-08-25 12:49:23 +0600

    update uber/interactive-shape

--- components/core/LocaleSwitcher.tsx
+++ components/core/LocaleSwitcher.tsx
@@ -1,32 +1,33 @@
 'use client';
-import { locales } from '@/lib/helper/locales';
 import { setUserLocale } from '@/lib/helper/server-func';
+import { cn } from '@/lib/utils';
 import { useLocale } from 'next-intl';
 import { startTransition } from 'react';
 
 export default function LocaleSwitcher() {
 	const locale = useLocale();
 	function onChange(e: any) {
-		const locale = e.target.value;
+		console.log('e.target', e.target.checked);
 		startTransition(() => {
-			setUserLocale(locale);
+			setUserLocale(e.target.checked ? 'bn' : 'en');
 		});
 	}
 	return (
-		<div className="relative">
-			<select
-				defaultValue={locale}
+		<label className="inline-flex items-center cursor-pointer">
+			<input
+				type="checkbox"
+				className="sr-only peer"
+				checked={locale === 'bn'}
 				onChange={onChange}
+			/>
+			<div
+				className={cn(
+					"relative w-11 h-6 bg-gray-200 rounded-full px-1 text-xs peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600",
+					locale === 'bn' ? 'text-white' : 'text-black text-right'
+				)}
 			>
-				{locales.map((item) => (
-					<option
-						key={item}
-						value={item}
-					>
-						{item}
-					</option>
-				))}
-			</select>
-		</div>
+				{locale}
+			</div>
+		</label>
 	);
 }
