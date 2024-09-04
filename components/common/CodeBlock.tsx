efaf3d5867da77da932f44f49c150135e9461175
Tareq <tareqnmd@gmail.com>
2024-09-04 18:12:58 +0600

    update

--- components/common/CodeBlock.tsx
+++ components/common/CodeBlock.tsx
@@ -1,16 +1,5 @@
-'use client';
-import { Prism } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-
 const CodeBlock = ({ codeString }: { codeString: string }) => {
-	return (
-		<Prism
-			language="javascript"
-			style={dark}
-		>
-			{codeString}
-		</Prism>
-	);
+	return <>{codeString}</>;
 };
 
 export default CodeBlock;
