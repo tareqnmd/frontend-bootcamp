586299bbb81877fd413886c14d7554a74cc3c7a3
Tareq <tareqnmd@gmail.com>
2024-09-08 12:18:50 +0600

    update questions

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
 					'antialiased',
 					bengaliFont.variable,
 					font.variable,
-					locale === 'en' ? 'font-english' : 'font-bengali test-leading'
+					locale === 'en' ? 'font-english' : 'font-bengali leading-normal'
 				)}
 			>
 				<AppLayout locale={locale}>{children}</AppLayout>
