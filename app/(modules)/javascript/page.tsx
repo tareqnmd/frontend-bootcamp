54266f15ce7bfe79de6ab8c7cce3d2353ce7337d
Tareq <tareqnmd@gmail.com>
2024-09-22 10:39:02 +0600

    javascript menu removed

--- app/(modules)/javascript/page.tsx
+++
@@ -1,9 +0,0 @@
-const page = () => {
-    return (
-        <>
-            
-        </>
-    );
-};
-
-export default page;
--- lib/menu.ts
+++ lib/menu.ts
@@ -6,19 +6,11 @@ export type MenuTypes = MenuType[];
 
 export const menus = [
 	{
-		key: 'javascript',
-		href: '/javascript',
-	},
-	{
 		key: 'interview_questions',
 		href: '/interview-questions',
 	},
 	{
 		key: 'challenge',
 		href: '/challenge',
-	},
-	{
-		key: 'problem_solving',
-		href: '/problem-solving',
 	},
 ];
